ea7a6f8ce99543374e1ed128b427f3820e268030
Samuel Ong <samuelongqixin@gmail.com>
2025-02-09 22:36:29 +0800

    minor update

--- lostandfound/chalicelib/connectHelper.py
+++ lostandfound/chalicelib/connectHelper.py
@@ -1,20 +1,23 @@
 import boto3
 import pymysql
+import os
 
 ssm_client = boto3.client('ssm')
 
+app_name = os.environ.get('APP_NAME')
+
 # Batch get parameters from SSM
-response = ssm_client.get_parameters(Names=['/lostandfound/rds_host', '/lostandfound/rds_user', '/lostandfound/rds_password', '/lostandfound/db_name'], WithDecryption=True)
+response = ssm_client.get_parameters(Names=[f'/{app_name}/rds_host', f'/{app_name}/rds_user', f'/{app_name}/rds_password', f'/{app_name}/db_name'], WithDecryption=True)
 ssm_dict = {param['Name']: param['Value'] for param in response['Parameters']}
 
 def create_connection():
     # RDS connection details from environment variables
-    HOST = ssm_dict['/lostandfound/rds_host']
-    USER = ssm_dict['/lostandfound/rds_user']
-    PASSWORD = ssm_dict['/lostandfound/rds_password']
-    DB_NAME = ssm_dict['/lostandfound/db_name']
+    HOST = ssm_dict[f'/{app_name}/rds_host']
+    USER = ssm_dict[f'/{app_name}/rds_user']
+    PASSWORD = ssm_dict[f'/{app_name}/rds_password']
+    DB_NAME = ssm_dict[f'/{app_name}/db_name']
 
     connection = pymysql.connect(host=HOST, user=USER, password=PASSWORD, database=DB_NAME, charset='utf8mb4',
-                                     cursorclass=pymysql.cursors.DictCursor, autocommit=True)
+                                 cursorclass=pymysql.cursors.DictCursor, autocommit=True)
 
     return connection
